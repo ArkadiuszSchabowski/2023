2a287a9694970cd98bb2121b80c8ee68cea98c1f
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2023-09-06 00:57:42 +0200

    Work with my page

--- OwnProjectTibia/mainPage.js
+++ OwnProjectTibia/mainPage.js
@@ -10,3 +10,41 @@ function ClearLogin() {
 function ClearPassword() {
   password.value = "";
 }
+
+
+let galleryDiv = document.getElementById("gallery");
+let currentImage = null; // Zmienna przechowująca referencję do obecnie wyświetlanego obrazka
+
+async function CreateDataOnJson() {
+  let urlApi = "https://random.dog/woof.json";
+  let response = await fetch(urlApi);
+  let data = await response.json();
+
+  // Sprawdź, czy URL kończy się na .mp4 i jeśli tak, pobierz kolejne dane
+  if (data.url.endsWith(".mp4") || data.url.endsWith(".webm")) {
+    CreateDataOnJson();
+    return;
+  }
+
+  GetUrlOnWebsite(data.url);
+}
+
+function GetUrlOnWebsite(data) {
+  // Usuń poprzedni obrazek, jeśli istnieje
+  if (currentImage) {
+    galleryDiv.removeChild(currentImage);
+  }
+
+  let simpleImg = document.createElement("img");
+  simpleImg.src = data;
+  console.log(data);
+  simpleImg.classList.add("imgGallery");
+  galleryDiv.appendChild(simpleImg);
+
+  currentImage = simpleImg; // Ustaw obecny obrazek na nowo dodany
+}
+
+CreateDataOnJson();
+
+galleryDiv.addEventListener("click", CreateDataOnJson);
+
